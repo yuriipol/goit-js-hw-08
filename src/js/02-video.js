03620c86e27669b1fa90ed9a135a5f675929976c
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-11 17:37:17 +0300

    finish task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player'; // импорт библиотек
+import throttle from 'lodash.throttle'; // импорт библиотек
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+//отслеживаем событие timeupdate и вешаем на фукуию throttle
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    console.log('played the video!');
+    //зписываем в localStorage из обьекта значение секунд data.seconds
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }),
+  500
+);
+//берем название ролика
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+//вызываем созданную нами функцию
+chekedSaveTime();
+
+function chekedSaveTime() {
+  //создаем перменную time и записываем в нее значение из localStorage
+  const time = localStorage.getItem('videoplayer-current-time');
+  if (time !== null) {
+    //если не равно null, то на player вешаем его встроенную фукцию setCurrentTime с переменной
+    //time, которую преобразуем в число с помощью Number
+    player.setCurrentTime(Number(time));
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('.feedback-form input');
+const textareaMessage = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', onFormSubmint);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextInput(); //вызов фукции, которая подставляет значения из localStorage
+
+function onFormSubmint(event) {
+  event.preventDefault();
+  //вывод в консоль обьекта введенных данных
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const dataObj = { email: email.value, message: message.value };
+
+  console.log(dataObj);
+  //очистка input and textarea
+  event.currentTarget.reset();
+  //очистка localStorage
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput() {
+  //делаем дистриктуризацию
+  const {
+    elements: { email, message },
+  } = form;
+  //сздаем обьект и присваиваем ему введенные значения input and textarea
+  const dataObj = { email: email.value, message: message.value };
+  //записываем обьект в localStorage, но при это с помощью JSON.stringify преобразуем его в строку
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataObj));
+}
+//фукции, которая подставляет значения из localStorage в input and textarea
+function populateTextInput() {
+  //создаем переменную, в которую записываем значение из localStorage
+  const savedText = localStorage.getItem('feedback-form-state');
+  //преобразуем значение в обьект
+  parseText = JSON.parse(savedText);
+  //если в localStorage есть savedText, то присваиваем input and textarea значения из обьекта parseText
+  if (savedText) {
+    inputEmail.value = parseText.email;
+    textareaMessage.value = parseText.message;
+  }
+}
